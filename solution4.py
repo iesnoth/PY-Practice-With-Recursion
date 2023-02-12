c64c938bb4e2cbeac74d541f8954bb2ea9960c0d
Victoria Beasley <iesnoth91@gmail.com>
2023-02-12 13:39:02 -0600

    finished hw

--- solution4.py
+++ solution4.py
@@ -1,1 +1,10 @@
-# Write code for algorithm 4 below
+# Python function called num_within() to check whether a number falls in a given range.
+# The function accepts the number, beginning of range, and end of range (inclusive) as arguments.
+
+def num_within(num,range_start,range_end):
+    if num > range_start and num <= range_end:
+        return True
+    else: return False
+
+print(num_within(1,4,6)) #returns false
+print(num_within(6,4,6)) #resturns true
--- solution5.py
+++ solution5.py
@@ -1,1 +1,18 @@
-# Write code for algorithm 5 below
+# function called pascal() that prints out the first n rows of Pascal's triangle.
+# The function accepts the number n, the number of rows to print
+
+# this solution is based on using the powers of 11 to build the triangle
+#doesn't do all the fancy math, but it is a lot easier for me to understand
+def pascal(n):
+    for i in range(n):
+        print(' '.join(map(str, str(11**i))))
+        # 11**i multiplies 11 to the power of the number(i)
+        # that number is then turned into a string by str
+        # that string is then mapped. The map() receives two arguments:
+            # the function to which the iterable object is passed
+            # the iterable variable.
+        #In this instance, the integer had to become a string first, because it wasn't iterable. The map then returns an object.
+        #.join combines all the object items into a string. In this instance, it's using the ' ' as a separator, but anything could be there.
+            #.join /has/ to have a separator
+
+pascal(5)
